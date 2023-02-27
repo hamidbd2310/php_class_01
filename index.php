a8ba1b57e491ec13866d65233f57ebc7b6362789
Abdul Hamid <hamidbd2310@gmail.com>
2023-02-27 17:27:18 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+
+<?php 
+// single line comment
+
+/*
+
+echo  for output
+multiline comment
+*/
+
+echo "Welcome to learn php"."<br/>";
+
+// variable 
+$number =1;
+$float =1.5;
+
+echo "serial:". $number."<br/>";
+echo $float."<br/>";
+
+//int  example 1, float 1.5, boolean true/false
+
+// array ,object 
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="save.php" method="post">
+        <input type="text"  name="username"/>
+        <button type="submit">SAVE</button>
+    </form>
+</body>
+</html>
