7adf9cf1f9824792aaa74e1b18d6501c7526f80f
Abdul Hamid <hamidbd2310@gmail.com>
2023-02-28 21:19:30 +0600

    Wood Cal

--- form.php
+++ form.php
@@ -8,12 +8,18 @@
     <title>Document</title>
 </head>
 <body>
-    <hi>Wood Calculator<h/>
+    <hi>Wood Calculator Size Wood<h/>
     <form action="submit.php" method="post">
         <input type="number"  name="num" placeholder="Length In Foot"/> <br/>
         <input type="number"  name="2num"placeholder="Width In Inch"/> <br/>
         <input type="number"  name="3num" placeholder="Thickness In Inch"/> <br/>
-        <button type="submit">SAVE</button>
+        <button type="submit">Total</button>
+    </form>
+    <hi>Wood Calculator Round Wood<h/>
+    <form action="submit.php" method="post">
+        <input type="number"  name="rnum" placeholder="Length In Foot"/> <br/>
+        <input type="number"  name="2rnum"placeholder="Round In Inch"/> <br/>
+        <button type="submit">Total</button>
     </form>
 </body>
 </html>
--- submit.php
+++ submit.php
@@ -3,7 +3,14 @@ $firstNumber=$_POST['num'];
 $secondNumber=$_POST['2num'];
 $ThirdNumber=$_POST['3num'];
 $totalCFT =$firstNumber*$secondNumber*$ThirdNumber;
-echo "Total CFT: " . $totalCFT/144;
+echo "Total Size CFT: " . $totalCFT/144;
+
+
+
+$rfirstNumber=$_POST['rnum'];
+$rsecondNumber=$_POST['2rnum'];
+$totalCFT =$rfirstNumber*$rsecondNumber*$rsecondNumber;
+echo "  Total Round CFT: " . $totalCFT/2304;
 // if(isset($number)) {
 
 //     echo "Empty NUmber";
