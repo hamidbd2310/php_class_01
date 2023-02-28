fdd673f3d372b5204c0b78fad611d55f8ce72fc4
Abdul Hamid <hamidbd2310@gmail.com>
2023-02-28 20:14:24 +0600

    php3

---
+++ submit.php
@@ -0,0 +1,16 @@
+<?php
+$firstNumber=$_POST['num'];
+$secondNumber=$_POST['2num'];
+$ThirdNumber=$_POST['3num'];
+echo "Total Sum ".$firstNumber+$secondNumber*$ThirdNumber;
+// if(isset($number)) {
+
+//     echo "Empty NUmber";
+// }
+// if($number%4==0) {
+// echo  "Leap Year";
+// }
+// else {
+//     echo  "Not";
+// }
+?>
